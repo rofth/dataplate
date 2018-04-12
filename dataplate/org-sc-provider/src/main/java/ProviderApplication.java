e0e1d606b7c01004d1d61fb335a91e6291ca995c
程超 <chengchao3206@dingtalk.com>
2018-04-12 17:54:41 +0800

    #[idea][master]更改apollo-client版本

--- dataplate/org-sc-provider/src/main/java/ProviderApplication.java
+++ dataplate/org-sc-provider/src/main/java/ProviderApplication.java
@@ -1,4 +1,3 @@
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
