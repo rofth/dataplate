47efe36412a840300fe25d37bce8e32a29ddaa30
程超 <chengchao3206@dingtalk.com>
2018-04-13 10:41:42 +0800

    #[idea][branch]更改branch版本

--- dataplate/org-sc-provider/src/main/java/ProviderApplication.java
+++ dataplate/org-sc-provider/src/main/java/ProviderApplication.java
@@ -24,6 +24,6 @@ public class ProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class,args);
-        System.out.printf("---");
+        System.out.printf("+++");
     }
 }
